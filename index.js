b37e83168e886fd3c60ac00f5397c3eabafc8ebb
Brennan Blair <bblair31@users.noreply.github.com>
2019-01-21 14:09:17 -0500

    solve linked-list-lab

---
+++ index.js
@@ -0,0 +1,68 @@
+function getName(node) {
+  return node.name
+}
+
+function headNode(linkedList, collection) {
+  return collection[linkedList]
+}
+
+function next(node, collection) {
+  let nextAddress = node.next
+
+  return collection[nextAddress]
+}
+
+function nodeAt(index, linkedList, collection) {
+  let currentNode = headNode(linkedList, collection)
+
+  for (let i = 0; i < index; i++) {
+    currentNode = next(currentNode, collection)
+  }
+  return currentNode
+}
+
+function addressAt(index, linkedList, collection) {
+  if (index !== 0) {
+    let node = nodeAt(index - 1, linkedList, collection)
+    return node.next
+  } else {
+    return linkedList
+  }
+}
+
+function indexAt(node, collection, linkedList) {
+  let currentNode = headNode(linkedList, collection)
+  let currentIndex = 0
+  while (currentNode != node) {
+    currentIndex++
+    currentNode = next(currentNode, collection)
+  }
+  return currentIndex
+}
+
+function insertNodeAt(index, newNodeAddress, linkedList, collection){
+  let previousNode = nodeAt(index - 1, linkedList, collection)
+  let subsequentNode = nodeAt(index, linkedList, collection)
+
+  let previousNodeIdx = indexAt(previousNode, collection, linkedList)
+  let subsequentNodeIdx = indexAt(subsequentNode, collection, linkedList)
+  let previousNodeAddress = addressAt(previousNode, linkedList, collection)
+  let subsequentNodeAddress = addressAt(subsequentNode, linkedList, collection)
+  console.log("previousNode", previousNode)
+  console.log("subsequentNode", subsequentNode)
+  console.log("previousNodeAddress", previousNodeAddress)
+  console.log("subsequentNodeAddress", subsequentNodeAddress)
+  previousNode.next = newNodeAddress
+  let newNode = collection[newNodeAddress]
+  newNode.next = subsequentNodeAddress
+}
+
+function deleteNodeAt(index, linkedList, collection){
+  let previousNode;
+  let currentNode = headNode(linkedList, collection);
+  for(let i = 0; i < index; i++){
+     previousNode = currentNode
+     currentNode = next(currentNode, collection);
+  }
+  previousNode.next = currentNode.next
+}
